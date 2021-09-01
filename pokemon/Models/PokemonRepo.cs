435c01e9c2729a4997df1fd3110b563fd84b34e9
andrewlin1368 <andrewlin1368@gmail.com>
2021-09-01 14:24:39 -0400

    pokemon repo and interface

---
+++ pokemon/Models/PokemonRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pokemon.Models
+{
+    public class PokemonRepo:IPokemonRepo
+    {
+    }
+}
