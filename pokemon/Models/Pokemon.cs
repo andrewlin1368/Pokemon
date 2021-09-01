5a8fe7d2127f42516278b297ceca0c52604b9fe8
andrewlin1368 <andrewlin1368@gmail.com>
2021-09-01 14:23:30 -0400

    pokemon class

---
+++ pokemon/Models/Pokemon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pokemon.Models
+{
+    public class Pokemon
+    {
+        [Key]
+        [Display(Name ="Pokemon ID")]
+        [Required(ErrorMessage ="Pokemon ID is required")]
+        public int PokemonID { get; set; }
+
+        [Key]
+        [Display(Name = "Pokemon Type")]
+        [Required(ErrorMessage = "Pokemon Type is required")]
+        public string PokemonType { get; set; }
+
+        [Key]
+        [Display(Name = "Pokemon Name")]
+        [Required(ErrorMessage = "Pokemon Name is required")]
+        public int PokemonName { get; set; }
+
+        [Key]
+        [Display(Name = "Pokemon Gender")]
+        [Required(ErrorMessage = "Pokemon Gender is required")]
+        public int PokemonGender { get; set; }
+
+    }
+}
