1a5d1f7a9cd37a44cdedea7e5871ae7f4f8b82ec
andrewlin1368 <andrewlin1368@gmail.com>
2021-09-01 14:19:51 -0400

    added database and connection

---
+++ pokemon/pokemon.sql
@@ -0,0 +1,8 @@
+﻿create table Pokemon(
+PokemonID int primary key,
+PokemonType varchar(20),
+PokemonName varchar(20),
+PokemonGender varchar(20),
+constraint checkgender check (PokemonGender in('Male','Female')))
+
+select * from Pokemon
